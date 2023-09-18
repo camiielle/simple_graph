7d3254f0f2adebf88c4328ab7a4f490d3a9d27e0
camiielle <camillalazzati@gmail.com>
2023-09-18 10:51:28 +0200

    removed build from repo

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/camillalazzati/tesi/graph
-# Build directory: /home/camillalazzati/tesi/graph/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(graph.t "/home/camillalazzati/tesi/graph/build/graph.t")
-set_tests_properties(graph.t PROPERTIES  _BACKTRACE_TRIPLES "/home/camillalazzati/tesi/graph/CMakeLists.txt;24;add_test;/home/camillalazzati/tesi/graph/CMakeLists.txt;0;")
-subdirs("Lyra")
--- build/Lyra/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/camillalazzati/tesi/graph/Lyra
-# Build directory: /home/camillalazzati/tesi/graph/build/Lyra
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
